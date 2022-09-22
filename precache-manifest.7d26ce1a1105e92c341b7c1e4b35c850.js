98046acf7b59029bdcb5c6df395a9d960f6d528a
Gena <hennadiibondarenko@gmail.com>
2022-09-21 18:34:59 -0600

    Updates

---
+++ precache-manifest.7d26ce1a1105e92c341b7c1e4b35c850.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08166f9f9c73dece4112a051906de68e",
+    "url": "/React-iNote/index.html"
+  },
+  {
+    "revision": "2c200f33a76a6875f1d2",
+    "url": "/React-iNote/static/js/2.bed29b7e.chunk.js"
+  },
+  {
+    "revision": "f850aafc7d1411242298",
+    "url": "/React-iNote/static/js/main.d1d3121f.chunk.js"
+  },
+  {
+    "revision": "997369e41256c6fd328e",
+    "url": "/React-iNote/static/js/runtime-main.dbabea5e.js"
+  }
+]);
